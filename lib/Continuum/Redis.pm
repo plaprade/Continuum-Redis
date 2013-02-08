1813567a3b29ba49ac9e8af19ed0fce2eb29d902
ciphermonk <ciphermonk@altera-vita.com>
2013-02-08 13:58:58 +0700

    Setting v0.0.1

--- lib/Continuum/Redis.pm
+++ lib/Continuum/Redis.pm
@@ -8,7 +8,7 @@ use namespace::autoclean;
 use Continuum;
 use Mojo::Redis;
 
-use version; our $VERSION = version->declare("v0.0.2"); 
+use version; our $VERSION = version->declare("v0.0.1"); 
 
 has redis => (
     is => 'ro',
